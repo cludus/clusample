3008d29a40fc52b3795554078d4e23d9b6bbdc5d
Gilberto <gilberto.vento@gmail.com>
2024-09-12 14:47:55 -0400

    adding elasticsearch

--- src/main/java/com/cludus/clugest/model/EsPost.java
+++ src/main/java/com/cludus/clugest/model/EsPost.java
@@ -4,11 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
+@Document(indexName="clugest_posts")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class EsPost {
+    @Id
     private String id;
+
+    @Field(type = FieldType.Text)
+    private String title;
+
+    @Field(type = FieldType.Text)
+    private String content;
 }
--- src/main/java/com/cludus/clugest/repositories/EsPostRepository.java
+++ src/main/java/com/cludus/clugest/repositories/EsPostRepository.java
@@ -1,9 +1,12 @@
 package com.cludus.clugest.repositories;
 
+import com.cludus.clugest.model.EsPost;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Profile("elasticsearch")
-public class EsPostRepository {
+public interface EsPostRepository extends ElasticsearchRepository<EsPost, String> {
+
 }
