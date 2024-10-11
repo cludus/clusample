6473586342a273172dc6b57b1831dfe265fbe72c
Michel Atucha <zp1ke@outlook.com>
2024-10-11 10:20:05 -0500

    mongodb tests

--- src/test/java/com/cludus/clugest/MongodbTests.java
+++ src/test/java/com/cludus/clugest/MongodbTests.java
@@ -1,10 +1,8 @@
 package com.cludus.clugest;
 
-import com.cludus.clugest.dtos.CassChatMessageReq;
 import com.cludus.clugest.dtos.MgoProductDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,43 +11,43 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("mongodb")
 class MongodbTests {
-	@Autowired
-	private TestRestTemplate rest;
+    private static final String DB_NAME = "clugest_products";
 
-	@Container
-	private static final GenericContainer CONTAINER = new GenericContainer(DockerImageName.parse("mongo:latest"))
-			.withExposedPorts(27017)
-			.withEnv("MONGO_INITDB_ROOT_USERNAME", "admin")
-			.withEnv("MONGO_INITDB_ROOT_PASSWORD", "admin")
-			.withEnv("MONGO_INITDB_DATABASE", "clugest_products");
+    @Autowired
+    private TestRestTemplate rest;
 
-	@DynamicPropertySource
-	static void configureProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.data.mongodb.uri", () -> String.format("mongodb://%s:%d/clugest_products", CONTAINER.getHost(), CONTAINER.getMappedPort(27017)));
-		registry.add("spring.data.mongodb.username", () -> "admin");
-		registry.add("spring.data.mongodb.password", () -> "admin");
-	}
+    @Container
+    private static final GenericContainer<MongoDBContainer> CONTAINER = new MongoDBContainer(
+        DockerImageName.parse("mongo:latest"))
+        .withExposedPorts(27017)
+        .withEnv("MONGO_INITDB_DATABASE", DB_NAME);
 
-	@BeforeAll
-	static void beforeAll() {
-		CONTAINER.start();
-	}
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        CONTAINER.start();
+        String connUri = String.format(
+            "mongodb://%s:%d/%s", CONTAINER.getHost(), CONTAINER.getMappedPort(27017), DB_NAME);
+        registry.add("spring.data.mongodb.uri", () -> connUri);
+        registry.add("spring.data.mongodb.username", () -> "admin");
+        registry.add("spring.data.mongodb.password", () -> "admin");
+    }
 
-	@AfterAll
-	static void afterAll() {
-		CONTAINER.stop();
-	}
+    @AfterAll
+    static void afterAll() {
+        CONTAINER.stop();
+    }
 
-	@Test
-	void doTest() {
-		var req = MgoProductDto.builder().name("product").build();
-		var result = rest.postForEntity("/mongo/product", req, MgoProductDto.class);
-		Assertions.assertThat(result.getStatusCode().is2xxSuccessful()).isTrue();
-	}
+    @Test
+    void doTest() {
+        var req = MgoProductDto.builder().name("product").build();
+        var result = rest.postForEntity("/mongo/product", req, MgoProductDto.class);
+        Assertions.assertThat(result.getStatusCode().is2xxSuccessful()).isTrue();
+    }
 }
