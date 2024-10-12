629b9e22e2ec450cb7d7d2a1e48ec6193f5a619e
Gilberto <gilberto.vento@gmail.com>
2024-10-12 15:57:54 -0400

    adding k8s with k3d

---
+++ k8s/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+path=$(realpath "$0")
+dir=$(dirname $path)
+
+## k3d cluster
+k3d cluster create --config k8s/cluster.yml
+
+## cert-manager
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.3/cert-manager.yaml
+
+## dashboard
+# helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade -f dashboard/values.yml --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+kubectl apply -f dashboard/admin.sec.yml
+# kubectl -n kubernetes-dashboard create token dashboard-admin
+# kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-web 8000
+
+## mariadb
+## see https://github.com/mariadb-operator/mariadb-operator
+# helm repo add mariadb-operator https://helm.mariadb.com/mariadb-operator
+# helm repo update
+helm install mariadb-operator mariadb-operator/mariadb-operator --namespace mariadb-operator --create-namespace
+helm install mariadb-operator-crds mariadb-operator/mariadb-operator-crds
+kubectl create secret generic mariadb-galera-root-password --from-literal=password=hn978ng9g9n43c34
+kubectl apply -f mariadb/mariadb-galera.yml
+kubectl port-forward svc/kubernetes-dashboard-web 8000
---
+++ k8s/stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+path=$(realpath "$0")
+dir=$(dirname $path)
+
