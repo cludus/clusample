c7089a0e509bd7bb2ecb3c57e7cca96455a12415
Gilberto <gilberto.vento@gmail.com>
2024-10-16 22:02:37 -0400

    fixing hostnames

---
+++ k8s/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+path=$(realpath "$0")
+dir=$(dirname $path)
+
+## cert-manager
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.3/cert-manager.yaml
+
+## dashboard
+# helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade -f dashboard/values.yml --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+kubectl apply -f dashboard/admin.sec.yml
+# kubectl -n kubernetes-dashboard create token dashboard-admin
+# kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy --address 127.10.1.3 8443:443
+# naviagte to https://dashboard.clugest.home:8443
+
+## mariadb
+## see https://github.com/mariadb-operator/mariadb-operator
+# helm repo add mariadb-operator https://helm.mariadb.com/mariadb-operator
+# helm repo update
+helm install mariadb-operator mariadb-operator/mariadb-operator --namespace mariadb-operator --create-namespace
+helm install mariadb-operator-crds mariadb-operator/mariadb-operator-crds
+kubectl create secret generic mariadb-galera-root-password --from-literal=password=hn978ng9g9n43c34
+kubectl apply -f mariadb/mariadb-galera.yml
+# kubectl port-forward svc/clugest-mariadb-svc 8181:80
--- k8s/start.sh
+++ k8s/start.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
+## add the following lines to /etc/hosts
+# 127.10.1.1 k8s.clugest.home
+# 127.10.1.2 clugest.home mariadb.clugest.home
+# 127.10.1.3 dashboard.clugest.home
+
 path=$(realpath "$0")
 dir=$(dirname $path)
 
 ## k3d cluster
 k3d cluster create --config cluster.yml
-
-## cert-manager
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.3/cert-manager.yaml
-
-## dashboard
-# helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm upgrade -f dashboard/values.yml --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
-kubectl apply -f dashboard/admin.sec.yml
-# kubectl -n kubernetes-dashboard create token dashboard-admin
-# kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
-
-## mariadb
-## see https://github.com/mariadb-operator/mariadb-operator
-# helm repo add mariadb-operator https://helm.mariadb.com/mariadb-operator
-# helm repo update
-helm install mariadb-operator mariadb-operator/mariadb-operator --namespace mariadb-operator --create-namespace
-helm install mariadb-operator-crds mariadb-operator/mariadb-operator-crds
-kubectl create secret generic mariadb-galera-root-password --from-literal=password=hn978ng9g9n43c34
-kubectl apply -f mariadb/mariadb-galera.yml
-# kubectl port-forward svc/clugest-mariadb-svc 8181:80
