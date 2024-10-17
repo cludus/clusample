d07f08b5ed26dcddaa0183e026421138f1742669
Gilberto <gilberto.vento@gmail.com>
2024-10-16 21:29:31 -0400

    fixing service in k8s

--- k8s/start.sh
+++ k8s/start.sh
@@ -14,7 +14,7 @@ kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/
 helm upgrade -f dashboard/values.yml --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
 kubectl apply -f dashboard/admin.sec.yml
 # kubectl -n kubernetes-dashboard create token dashboard-admin
-# kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-web 8000
+# kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
 
 ## mariadb
 ## see https://github.com/mariadb-operator/mariadb-operator
@@ -24,4 +24,4 @@ helm install mariadb-operator mariadb-operator/mariadb-operator --namespace mari
 helm install mariadb-operator-crds mariadb-operator/mariadb-operator-crds
 kubectl create secret generic mariadb-galera-root-password --from-literal=password=hn978ng9g9n43c34
 kubectl apply -f mariadb/mariadb-galera.yml
-kubectl port-forward svc/kubernetes-dashboard-web 8000
+# kubectl port-forward svc/clugest-mariadb-svc 8181:80
--- k8s/stop.sh
+++ k8s/stop.sh
@@ -3,3 +3,4 @@
 path=$(realpath "$0")
 dir=$(dirname $path)
 
+k3d cluster delete clugest
